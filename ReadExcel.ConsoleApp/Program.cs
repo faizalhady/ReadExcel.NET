d8addde1d981c0e07b13d9ce86d42084fc3a1776
faizalhady <syedfaizalhady@gmail.com>
2024-10-12 03:10:48 +0800

    feat: add functionality to read Excel files from directory and log file count

--- ReadExcel.ConsoleApp/Program.cs
+++ ReadExcel.ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ReadExcel.ConsoleApp.Services;
+
+namespace ReadExcel.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Users\syedf\Desktop\COMPLETE LOG DIRECTORY";  // Update this to your actual directory path
+
+            // Get the list of Excel files using the FileService (Corrected)
+            var excelFiles = FileService.GetExcelFiles(directoryPath);
+
+
+            // Optionally, log the names of each file
+            foreach (var file in excelFiles)
+            {
+                Console.WriteLine($"Found file: {Path.GetFileName(file)}");
+            }
+            Console.WriteLine($"Number of Excel files found: {excelFiles.Length}");
+
+            // Wait for the user to press a key before closing the console
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
